1dc204beb4a6039161c31bf52abb639273430354
Sean McGillis <slcidevice@yahoo.com>
2024-01-18 18:33:40 -0700

    Got the icon to show on the button

--- src/navigation/BottomTabs.tsx
+++ src/navigation/BottomTabs.tsx
@@ -4,13 +4,27 @@ import SettingsScreen from '../screens/SettingsScreen';
 import StatsScreen from '../screens/StatsScreen';
 import ExportScreen from '../screens/ExportScreen';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import IconButton from '../components/IconButton';
 
 const Tab = createBottomTabNavigator();
 
 export default function BottomTabs() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <IconButton
+              buttonName="List"
+              iconName="home"
+              isActive={true}
+              onToggleView={() => {}}
+            />
+          )
+        }}
+      />
       <Tab.Screen name="Stats" component={StatsScreen} />
       <Tab.Screen name="Export" component={ExportScreen} />
       <Tab.Screen name="Settings" component={SettingsScreen} />
