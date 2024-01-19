650029485f228bfac8183dc7565ffb7b47d208bd
Sean McGillis <slcidevice@yahoo.com>
2024-01-18 22:19:02 -0700

    Simplification

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -9,13 +9,7 @@ type ButtonProps = {
 
 function IconButton({focused, color, size}: ButtonProps): React.JSX.Element {
   console.log(focused);
-  return (
-    <MaterialCommunityIcons
-      name="home"
-      color={focused ? color : 'grey'}
-      size={size}
-    />
-  );
+  return <MaterialCommunityIcons name="home" color={color} size={size} />;
 }
 
 export default IconButton;
