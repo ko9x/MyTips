0c5a65a740e18283826d9448d6d677d34cbfaac1
Sean McGillis <slcidevice@yahoo.com>
2024-02-24 20:34:53 -0700

    Removed the inline styles in the ListItem component

--- src/components/ListItems.tsx
+++ src/components/ListItems.tsx
@@ -24,56 +24,21 @@ export default function ListItems({
   return itemArr.map((item: any) => (
     <View key={item.id}>
       <Pressable onPress={() => pressHandler(item.id)}>
-        <View
-          style={{
-            borderColor: Colors.lighterGrey,
-            borderWidth: 1,
-            borderRadius: 20,
-            marginVertical: 10,
-            paddingVertical: 5,
-            paddingHorizontal: 10,
-          }}>
-          <View
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'space-evenly',
-              alignItems: 'center',
-              marginVertical: 10,
-            }}>
+        <View style={styles.listItemContainer}>
+          <View style={styles.listItemTopSectionContainer}>
             <Text>job: {item.job}</Text>
             <Text>section: {item.section}</Text>
           </View>
-          <View
-            style={{
-              borderBottomColor: Colors.lighterGrey,
-              borderBottomWidth: 1,
-              marginLeft: 5,
-              marginRight: 5,
-            }}
-          />
-          <View
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'space-evenly',
-              alignItems: 'center',
-              paddingTop: 10,
-            }}>
-            <View
-              style={{
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
+          <View style={styles.listItemHorizontalDivider} />
+          <View style={styles.listItemBottomSectionContainer}>
+            <View style={styles.bottomSectionItemContainer}>
               <Text>tips</Text>
               <Text>{toDollars(findCorrectItem(totalMoney, item.id))}</Text>
             </View>
             <View>
               <Text style={styles.tipSummaryDivider}>|</Text>
             </View>
-            <View
-              style={{
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
+            <View style={styles.bottomSectionItemContainer}>
               <Text>time worked</Text>
               <Text>
                 {toHoursAndMinutes(item.time).hours}{' '}
@@ -86,11 +51,7 @@ export default function ListItems({
             <View>
               <Text style={styles.tipSummaryDivider}>|</Text>
             </View>
-            <View
-              style={{
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
+            <View style={styles.bottomSectionItemContainer}>
               <Text>per hour</Text>
               <Text>
                 {toPerHour(item.time, findCorrectItem(totalMoney, item.id))}
@@ -104,5 +65,35 @@ export default function ListItems({
 }
 
 const styles = StyleSheet.create({
-  tipSummaryDivider: {color: Colors.lighterGrey},
+  listItemContainer: {
+    borderColor: Colors.lightGrey,
+    borderWidth: 1,
+    borderRadius: 20,
+    marginVertical: 10,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+  },
+  listItemTopSectionContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  listItemHorizontalDivider: {
+    borderBottomColor: Colors.lightGrey,
+    borderBottomWidth: 1,
+    marginLeft: 5,
+    marginRight: 5,
+  },
+  listItemBottomSectionContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    paddingTop: 10,
+  },
+  bottomSectionItemContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  tipSummaryDivider: {color: Colors.lightGrey},
 });
