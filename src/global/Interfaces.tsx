3d2c06b98424d131a4b24e898876d8536a86d305
Sean McGillis <slcidevice@yahoo.com>
2024-02-24 20:27:29 -0700

    Created a global interface file

---
+++ src/global/Interfaces.tsx
@@ -0,0 +1,13 @@
+export interface ResDataObj {
+  cash: number;
+  credit: number;
+  hourly_rate: number;
+  id: number;
+  job: string;
+  note: string;
+  section: string;
+  time: number;
+  tip_in: number;
+  tip_out: number;
+  total_sales: number;
+}
