57106405ae425ace22a382d86164c9bbc9776a66
Sean McGillis <slcidevice@yahoo.com>
2024-03-08 21:29:31 -0700

    Disable font scaling

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {Text, TextInput} from 'react-native';
 
 AppRegistry.registerComponent(appName, () => App);
+
+if (Text.defaultProps == null) {
+  Text.defaultProps = {};
+  Text.defaultProps.allowFontScaling = false;
+}
+
+if (TextInput.defaultProps == null) {
+  TextInput.defaultProps = {};
+  TextInput.defaultProps.allowFontScaling = false;
+}
